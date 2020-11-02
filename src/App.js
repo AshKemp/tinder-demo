e394944433d1683c5e700c64b833500f77175dd0
Ashwin Km <kmuraliashwin@gmail.com>
2020-11-02 20:16:35 +0530

    Included TinderCards Component in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Header } from "./Header";
+import TinderCards from "./TinderCards";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       {/* {Header} */}
       <Header />
       {/* {Tinder Cards} */}
+      <TinderCards />
       {/* {SwipeButtons} */}
     </div>
   );
